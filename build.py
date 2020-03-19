4c3a185285464da1dae0740276d4594d7b383dde
Knife <konrad@no.tantoo>
2020-03-19 10:23:53 +0100

    Split travis builds on shared option

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(docker_32_images=True, pip_install=["mako"])
-    builder.add_common_builds(shared_option_name="ogre-procedural:shared")
+    builder.add_common_builds()
     builder.run()
